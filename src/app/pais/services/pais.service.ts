529aa4584d84feeaf14cbbcfd6f8d124ffba2849
zoila quintero <zoilita205@hotmail.com>
2023-03-07 20:52:11 -0500

    segundo  comit

---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+  private apiUlr:string='https://restcountries.com/v2/';
+  constructor(private http:HttpClient) { }
+  buscarPais(termino:string):Observable <Country[]>{
+    const url = `${this.apiUlr}/name/${termino}`;
+    return this.http.get<Country[]>(url);
+  }
+}
