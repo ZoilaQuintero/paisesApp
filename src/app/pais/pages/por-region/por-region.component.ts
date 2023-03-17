78163976bc8738535b7b54082163fc6f2fbc928c
zoila quintero <zoilita205@hotmail.com>
2023-03-16 21:13:22 -0500

    sesto commit

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styles:[
+    `
+    button{
+      margin-right:5px;
+    }
+    `
+  ]
 })
 export class PorRegionComponent {
+  regiones: string[]=['africa','america','asia','europa','oseania'];
+  reginActiva:string='';
 
+  getClassCss(region:string):string{
+    return (region===this.reginActiva) ? 'btn btn-primary':'btn btn-outline-primary';
+  }
+  activarRegion(region:string){
+    this.reginActiva=region;
+    //TODO: hace el llamado al servicio
+  }
 }
